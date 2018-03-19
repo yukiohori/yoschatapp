59df1949aeeb8fb717af6f9a954c6b3f91bde329
YOS <yuki8963@gmail.com>
2018-03-19 12:05:30 +0900

    profile added

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Badge, Container, Header, Content, List, ListItem, Left, Body, Right, Button, Icon, Title, Thumbnail, Text } from 'native-base';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -10,7 +11,10 @@ export default class SettingsScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-      
+        <View style={styles.profile}>
+          <Thumbnail large source={{ uri: 'https://www.buira.org/assets/images/shared/default-profile.png' }} />
+          <Text style={{marginTop: 10}}>Username</Text>
+        </View>
       </ScrollView>
     );
   }
@@ -22,4 +26,9 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  profile:{
+    marginTop: 30,
+    flex: 1,
+    alignItems: 'center',
+  }
 });
